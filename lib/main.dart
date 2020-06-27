1c337a80ee04bdad255dbb41533384799583daf8
festa78 <nonamehorses78@gmail.com>
2020-06-27 09:15:36 +0900

    Add side bar and text box

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'package:myapp/widgets/nav_drawer.dart';
+
 void main() {
   runApp(MyApp());
 }
@@ -80,56 +82,38 @@ class _MyHomePageState extends State<MyHomePage> {
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: ListView.separated(
-        padding: const EdgeInsets.all(8),
-        itemCount: widget.entries.length,
-        itemBuilder: (BuildContext context, int index) {
-          return Container(
-            height: 30,
-            color: Colors.amber[widget.colorCodes[index]],
-            // child: Center(child: Text('Entry ${widget.entries[index]}')),
-            child: Row(children: <Widget>[
-              Expanded(child: Text('Entry ${widget.entries[index]}')),
-              Expanded(child: Text('Tag ${widget.tags[index]}', textAlign: TextAlign.right)),
-            ]),
-          );
-        },
-        separatorBuilder: (BuildContext context, int index) => Divider(
-          color: Colors.grey[300],
-          height: 5,
-          thickness: 2,
-        ),
+      drawer: NavDrawer(),
+      body: Column(
+        children: <Widget>[
+          TextField(
+            decoration: InputDecoration(
+              border: InputBorder.none,
+              hintText: 'Enter a search term'
+            ),
+          ),
+          ListView.separated(
+            padding: const EdgeInsets.all(8),
+            scrollDirection: Axis.vertical,
+            shrinkWrap: true,
+            itemCount: widget.entries.length,
+            itemBuilder: (BuildContext context, int index) {
+              return Container(
+                height: 30,
+                color: Colors.amber[widget.colorCodes[index]],
+                child: Row(children: <Widget>[
+                  Expanded(child: Text('Entry ${widget.entries[index]}')),
+                  Expanded(child: Text('Tag ${widget.tags[index]}', textAlign: TextAlign.right)),
+                ]),
+              );
+            },
+            separatorBuilder: (BuildContext context, int index) => Divider(
+              color: Colors.grey[300],
+              height: 5,
+              thickness: 2,
+            ),
+          ),
+        ]
       ),
-      // body: Center(
-      //   // Center is a layout widget. It takes a single child and positions it
-      //   // in the middle of the parent.
-      //   child: Column(
-      //     // Column is also a layout widget. It takes a list of children and
-      //     // arranges them vertically. By default, it sizes itself to fit its
-      //     // children horizontally, and tries to be as tall as its parent.
-      //     //
-      //     // Invoke "debug painting" (press "p" in the console, choose the
-      //     // "Toggle Debug Paint" action from the Flutter Inspector in Android
-      //     // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-      //     // to see the wireframe for each widget.
-      //     //
-      //     // Column has various properties to control how it sizes itself and
-      //     // how it positions its children. Here we use mainAxisAlignment to
-      //     // center the children vertically; the main axis here is the vertical
-      //     // axis because Columns are vertical (the cross axis would be
-      //     // horizontal).
-      //     mainAxisAlignment: MainAxisAlignment.center,
-      //     children: <Widget>[
-      //       Text(
-      //         'You have pushed the button this many times:',
-      //       ),
-      //       Text(
-      //         '$_counter',
-      //         style: Theme.of(context).textTheme.headline4,
-      //       ),
-      //     ],
-      //   ),
-      // ),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
