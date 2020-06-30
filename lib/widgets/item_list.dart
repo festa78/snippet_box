cfa73c0e4cc969f78c1ae277f795c6df4e136bee
festa78 <nonamehorses78@gmail.com>
2020-06-30 20:18:18 +0900

    Launch URL

--- lib/widgets/item_list.dart
+++ lib/widgets/item_list.dart
@@ -1,7 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class ItemList extends StatelessWidget {
+
+  _launchURL(url) async {
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw 'Could not Launch $url';
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return StreamBuilder<QuerySnapshot>(
@@ -15,10 +25,14 @@ class ItemList extends StatelessWidget {
             return new ListView(
               shrinkWrap: true,
               children: snapshot.data.documents.map((DocumentSnapshot document) {
-                return new Card(child: ListTile(
-                  title: new Text(document['title']),
-                  subtitle: new Text(document['tags'].join(',')),
-                ));
+                return new GestureDetector(
+                  onTap: () => _launchURL(document['uri']),
+                  child: new Card(child: ListTile(
+                    title: new Text(document['title']),
+                    subtitle: new Text(document['tags'].join(',')),
+                  )
+                )
+                );
               }).toList(),
             );
         }
