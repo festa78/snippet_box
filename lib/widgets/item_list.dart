eaf8e1691b65762c6977067cc3c81ba2f2f58f87
festa78 <nonamehorses78@gmail.com>
2020-07-12 16:29:01 +0900

    show text in new page

--- lib/widgets/item_list.dart
+++ lib/widgets/item_list.dart
@@ -3,12 +3,17 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 class ItemList extends StatelessWidget {
-
-  _launchURL(url) async {
-    if (await canLaunch(url)) {
-      await launch(url);
+  _navigate(context, document) async {
+    if (await canLaunch(document['uri'])) {
+      await launch(document['uri']);
     } else {
-      throw 'Could not Launch $url';
+      await Navigator.push(
+          context,
+          MaterialPageRoute(
+              builder: (
+            context,
+          ) =>
+                  SecondRoute()));
     }
   }
 
@@ -17,26 +22,38 @@ class ItemList extends StatelessWidget {
     return StreamBuilder<QuerySnapshot>(
       stream: Firestore.instance.collection('test_list').snapshots(),
       builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
-        if (snapshot.hasError)
-          return new Text('Error: ${snapshot.error}');
+        if (snapshot.hasError) return new Text('Error: ${snapshot.error}');
         switch (snapshot.connectionState) {
-          case ConnectionState.waiting: return new Text('Loading...');
+          case ConnectionState.waiting:
+            return new Text('Loading...');
           default:
             return new ListView(
               shrinkWrap: true,
-              children: snapshot.data.documents.map((DocumentSnapshot document) {
+              children:
+                  snapshot.data.documents.map((DocumentSnapshot document) {
                 return new GestureDetector(
-                  onTap: () => _launchURL(document['uri']),
-                  child: new Card(child: ListTile(
-                    title: new Text(document['title']),
-                    subtitle: new Text(document['tags'].join(',')),
-                  )
-                )
-                );
+                    onTap: () => _navigate(context, document),
+                    child: new Card(
+                        child: ListTile(
+                      title: new Text(document['title']),
+                      subtitle: new Text(document['tags'].join(',')),
+                    )));
               }).toList(),
             );
         }
       },
+    );
+  }
+}
+
+class SecondRoute extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Second Route"),
+      ),
+      body: Center(child: Text('Go back!')),
     );
   }
 }
