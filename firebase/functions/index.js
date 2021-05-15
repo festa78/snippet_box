117f5b282fdf26baf430f89140b84510401ccd98
festa78 <nonamehorses78@gmail.com>
2021-05-15 12:27:18 +0900

    Send snippet to text search engine

--- firebase/functions/index.js
+++ firebase/functions/index.js
@@ -15,6 +15,15 @@ const { JSDOM } = require('jsdom');
 
 var fireStore = admin.firestore()
 
+const algoliasearch = require('algoliasearch');
+
+const ALGOLIA_ID = functions.config().algolia.app_id;
+const ALGOLIA_ADMIN_KEY = functions.config().algolia.api_key;
+const ALGOLIA_SEARCH_KEY = functions.config().algolia.search_key;
+
+const ALGOLIA_INDEX_NAME = 'snippets';
+const client = algoliasearch(ALGOLIA_ID, ALGOLIA_ADMIN_KEY);
+
 function firebaseGetUserByEmail(email) {
   return admin.auth().getUserByEmail(email)
     .then((userRecord) => {
@@ -92,7 +101,26 @@ exports.addMessage = functions.https.onRequest(async (req, res) => {
   }
 });
 
-exports.tagListOnUpdate = functions.firestore
+exports.snippetsOnCreated = functions.firestore
+  .document('user_data/{userId}/snippets/{docId}')
+  .onCreate((snap, context) => {
+    // Get the note document
+    const snippet = snap.data();
+
+    // Add an 'objectID' field which Algolia requires
+    snippet.objectID = context.params.docId;
+
+    // Write to the algolia index
+    const index = client.initIndex(ALGOLIA_INDEX_NAME);
+    return index.saveObject(snippet)
+      .then(() => console.log('Add snippet to Algoria'))
+      .catch(error => {
+        console.error('Error to add snippet to Algoria', error);
+        process.exit(1);
+      });
+});
+
+exports.snippetsOnUpdate = functions.firestore
   .document('user_data/{userId}/snippets/{docId}')
   .onUpdate((change, context) => {
     const newValue = change.after.data();
@@ -158,9 +186,21 @@ exports.tagListOnUpdate = functions.firestore
         })
         .catch((error) => console.log('error tag update:', error));
       });
+
+    // Add an 'objectID' field which Algolia requires
+    newValue.objectID = context.params.docId;
+
+    // Write to the algolia index
+    const index = client.initIndex(ALGOLIA_INDEX_NAME);
+    return index.saveObject(newValue)
+      .then(() => console.log('Update snippet on Algoria'))
+      .catch(error => {
+        console.error('Error to update snippet to Algoria', error);
+        process.exit(1);
+      });
   })
 
-exports.testListOnDelete = functions.firestore
+exports.snippetsOnDelete = functions.firestore
   .document('user_data/{userId}/snippets/{docId}')
   .onDelete((snapshot, context) => {
     const tagListRef = fireStore.collection('user_data/' + context.params.userId + '/tags');
@@ -194,6 +234,22 @@ exports.testListOnDelete = functions.firestore
           })
           .catch((error) => console.log('error tag update:', error));
     });
+
+    // Get Algolia's objectID from the Firebase object key
+    const objectID = context.params.docId;
+
+    // Remove the object from Algolia
+    const index = client.initIndex(ALGOLIA_INDEX_NAME);
+    index
+      .deleteObject(objectID)
+      .then(() => {
+        console.log('Firebase object deleted from Algolia', objectID);
+        return;
+      })
+      .catch(error => {
+        console.error('Error when deleting contact from Algolia', error);
+        process.exit(1);
+      });
   })
 
 function getHtmlTitle(url) {
@@ -202,24 +258,96 @@ function getHtmlTitle(url) {
       .then(response => response.text())
       .then(data => {
         const dom = new JSDOM(data);
-        resolve(dom.window.document.title);
+        return resolve(dom.window.document.title);
       })
       .catch(error => reject(error));
   });
 }
 
 exports.sendUrlToDb = functions.https.onCall((data, context) => {
-    functions.logger.info("send URL to DB!", { structuredData: true });
-    const userId = data.uid;
-    const uri = data.uri;
+  functions.logger.info("send URL to DB!", { structuredData: true });
+  const userId = data.uid;
+  const uri = data.uri;
 
-    return getHtmlTitle(uri)
-      .then(title => fireStore.collection('user_data/' + userId + '/snippets').doc().set({
-        tags: ['__all__'],
-        timestamp: admin.firestore.FieldValue.serverTimestamp(),
-        uri: uri,
-        title: title
-    }, { merge: true }))
-      .then(() => console.log('added uri'))
-      .catch((error) => console.log('error adding uri:', error));
+  return getHtmlTitle(uri)
+    .then(title => fireStore.collection('user_data/' + userId + '/snippets').doc().set({
+      tags: ['__all__'],
+      timestamp: admin.firestore.FieldValue.serverTimestamp(),
+      uri: uri,
+      title: title
+  }, { merge: true }))
+    .then(() => console.log('added uri'))
+    .catch((error) => console.log('error adding uri:', error));
 });
+
+// [START get_firebase_user]
+async function getFirebaseUser(req, res, next) {
+  functions.logger.log('Check if request is authorized with Firebase ID token');
+
+  if (!req.headers.authorization || !req.headers.authorization.startsWith('Bearer ')) {
+    functions.logger.error(
+      'No Firebase ID token was passed as a Bearer token in the Authorization header.',
+      'Make sure you authorize your request by providing the following HTTP header:',
+      'Authorization: Bearer <Firebase ID Token>'
+    );
+    return res.sendStatus(403);
+  }
+
+  let idToken;
+  if (req.headers.authorization && req.headers.authorization.startsWith('Bearer ')) {
+    functions.logger.log("Found 'Authorization' header");
+    idToken = req.headers.authorization.split('Bearer ')[1];
+  }
+
+  try {
+    const decodedIdToken = await admin.auth().verifyIdToken(idToken);
+    functions.logger.log('ID Token correctly decoded', decodedIdToken);
+    req.user = decodedIdToken;
+    return next();
+  } catch(error) {
+    functions.logger.error('Error while verifying Firebase ID token:', error);
+    return res.status(403).send('Unauthorized');
+  }
+}
+// [END get_firebase_user]
+
+// [START get_algolia_user_token]
+// This complex HTTP function will be created as an ExpressJS app:
+// https://expressjs.com/en/4x/api.html
+const app = require('express')();
+
+// We'll enable CORS support to allow the function to be invoked
+// from our app client-side.
+app.use(require('cors')({origin: true}));
+
+// Then we'll also use a special 'getFirebaseUser' middleware which
+// verifies the Authorization header and adds a `user` field to the
+// incoming request:
+// https://gist.github.com/abeisgoat/832d6f8665454d0cd99ef08c229afb42
+app.use(getFirebaseUser);
+
+// Add a route handler to the app to generate the secured key
+app.get('/', (req, res) => {
+  // @ts-ignore
+  const uid = req.user.uid;
+
+  // Create the params object as described in the Algolia documentation:
+  // https://www.algolia.com/doc/guides/security/api-keys/#generating-api-keys
+  const params = {
+    // This filter ensures that only documents where author == uid will be readable
+    // filters: `author:${uid}`,
+    // We also proxy the uid as a unique token for this key.
+    userToken: uid,
+  };
+
+  // Call the Algolia API to generate a unique key based on our search key
+  const key = client.generateSecuredApiKey(ALGOLIA_SEARCH_KEY, params);
+
+  // Then return this key as {key: '...key'}
+  res.json({key});
+});
+
+// Finally, pass our ExpressJS app to Cloud Functions as a function
+// called 'getSearchKey';
+exports.getSearchKey = functions.https.onRequest(app);
+// [END get_algolia_user_token]
