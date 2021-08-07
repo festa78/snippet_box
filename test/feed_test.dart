acf3a64c003b77c0449fb5e6af93b149d68db942
festa78 <nonamehorses78@gmail.com>
2021-08-07 14:43:27 +0900

    Add rss feed widget test for FeedListTile

--- test/feed_test.dart
+++ test/feed_test.dart
@@ -13,39 +13,76 @@ import 'package:xml/xml.dart';
 import 'package:myapp/widgets/feed.dart';
 
 void main() {
-  testWidgets('FeedListTile: initialization', (WidgetTester tester) async {
-    final String atomXml = '''<?xml version="1.0"?>
-    <entry>
-      <id>dummy_id</id>
-      <title>dummy_title</title>
-      <updated>2021-08-06T02:41:16.782Z</updated>
-      <link href="https://example.com/" />
-    </entry>''';
+  group('FeedListTile: initialization', () {
+    testWidgets('atom feed', (WidgetTester tester) async {
+      final String atomXml = '''<?xml version="1.0"?>
+      <entry>
+        <id>dummy_id</id>
+        <title>dummy_title</title>
+        <updated>2021-08-06T02:41:16.782Z</updated>
+        <link href="https://example.com/" />
+      </entry>''';
 
-    final xmlDocument = XmlDocument.parse(atomXml);
-    final atomItem = AtomItem.parse(xmlDocument.getElement('entry'));
-    final feedItemAndTime = FeedItemAndTime(atomItem);
-    print(feedItemAndTime.uri);
+      final xmlDocument = XmlDocument.parse(atomXml);
+      final atomItem = AtomItem.parse(xmlDocument.getElement('entry'));
+      final feedItemAndTime = FeedItemAndTime(atomItem);
+      print(feedItemAndTime.uri);
 
-    // Build our app and trigger a frame.
-    final sut = MediaQuery(
-      data: MediaQueryData(),
-      child: MaterialApp(
-        home: Scaffold(
-          body: FeedListTile(feedItemAndTime: feedItemAndTime),
+      // Build our app and trigger a frame.
+      final sut = MediaQuery(
+        data: MediaQueryData(),
+        child: MaterialApp(
+          home: Scaffold(
+            body: FeedListTile(feedItemAndTime: feedItemAndTime),
+          ),
         ),
-      ),
-    );
-    await tester.pumpWidget(sut);
+      );
+      await tester.pumpWidget(sut);
 
-    // Verify it shows feed title properly.
-    expect(find.text('dummy_title'), findsOneWidget);
+      // Verify it shows feed title properly.
+      expect(find.text('dummy_title'), findsOneWidget);
 
-    // Tap the feed and show up the url.
-    await tester.tap(find.text('dummy_title'));
-    await tester.pumpAndSettle();
+      // Tap the feed and show up the url.
+      await tester.tap(find.text('dummy_title'));
+      await tester.pumpAndSettle();
 
-    // Verify that it switches to EasyWebView.
-    expect(find.byKey(Key('dummy_title')), findsNWidgets(2));
+      // Verify that it switches to EasyWebView.
+      expect(find.byKey(Key('dummy_title')), findsNWidgets(2));
+    });
+
+    testWidgets('rss feed', (WidgetTester tester) async {
+      final String rssXml = '''<?xml version="1.0"?>
+      <item>
+        <title>dummy_title</title>
+        <pubDate>2021-08-06T02:41:16.782Z</pubDate>
+        <link>"https://example.com/"</link>
+      </item>''';
+
+      final xmlDocument = XmlDocument.parse(rssXml);
+      final rssItem = RssItem.parse(xmlDocument.getElement('item'));
+      final feedItemAndTime = FeedItemAndTime(rssItem);
+      print(feedItemAndTime.uri);
+
+      // Build our app and trigger a frame.
+      final sut = MediaQuery(
+        data: MediaQueryData(),
+        child: MaterialApp(
+          home: Scaffold(
+            body: FeedListTile(feedItemAndTime: feedItemAndTime),
+          ),
+        ),
+      );
+      await tester.pumpWidget(sut);
+
+      // Verify it shows feed title properly.
+      expect(find.text('dummy_title'), findsOneWidget);
+
+      // Tap the feed and show up the url.
+      await tester.tap(find.text('dummy_title'));
+      await tester.pumpAndSettle();
+
+      // Verify that it switches to EasyWebView.
+      expect(find.byKey(Key('dummy_title')), findsNWidgets(2));
+    });
   });
 }
