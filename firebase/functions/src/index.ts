c47b5f7053fc74ad7002f143e31c1e01e0f36e80
festa78 <nonamehorses78@gmail.com>
2021-11-06 11:20:12 +0900

    Run a first typescript test

--- firebase/functions/src/index.ts
+++ firebase/functions/src/index.ts
@@ -1,5 +1,8 @@
+import * as admin from 'firebase-admin';
 import * as functions from 'firebase-functions';
 import fetch, { Response } from 'node-fetch';
+
+admin.initializeApp();
 
 exports.getRssContent = functions.https.onCall(
   (data: string, context: functions.https.CallableContext) => {
@@ -14,5 +17,21 @@ exports.getRssContent = functions.https.onCall(
       .catch((error: functions.https.FunctionsErrorCode) => {
         throw new functions.https.HttpsError(error, `Error fetching ${data}`);
       });
+  }
+);
+
+exports.getRssContent = functions.https.onCall(
+  (context: functions.https.CallableContext) => {
+    if (!context.auth) {
+      throw new functions.https.HttpsError('permission-denied', 'Auth Error');
+    }
+
+    const userDataRef = admin.firestore().collection('user_data');
+
+    userDataRef.get().then((snapshot) => {
+      snapshot.forEach((doc) => {
+        console.log(doc.id);
+      });
+    });
   }
 );
---
+++ firebase/functions/test/index.spec.ts
@@ -0,0 +1,8 @@
+import { describe, it } from 'mocha';
+import { assert } from 'chai';
+
+describe('test', () => {
+  it('test it', () => {
+    assert.isTrue(true);
+  });
+});
