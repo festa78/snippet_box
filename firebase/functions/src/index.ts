3fc3db0f1b92c2ade715c79206c0f621862b1d27
festa78 <nonamehorses78@gmail.com>
2021-11-14 11:13:22 +0900

    Add function to store RSS item

--- firebase/functions/src/index.ts
+++ firebase/functions/src/index.ts
@@ -1,6 +1,7 @@
 import * as admin from 'firebase-admin';
 import * as functions from 'firebase-functions';
-import fetch, { Response } from 'node-fetch';
+import fetch from 'node-fetch';
+import Parser from 'rss-parser';
 
 admin.initializeApp();
 
@@ -10,13 +11,9 @@ export const getRssContent = functions.https.onCall(
       throw new functions.https.HttpsError('permission-denied', 'Auth Error');
     }
 
-    return fetch(data)
-      .then((response: Response) => {
-        return response.text();
-      })
-      .catch((error: functions.https.FunctionsErrorCode) => {
-        throw new functions.https.HttpsError(error, `Error fetching ${data}`);
-      });
+    return fetch(data).then((response) => {
+      return response.text();
+    });
   }
 );
 
@@ -26,12 +23,63 @@ export const updateRssContent = functions.https.onCall(
       throw new functions.https.HttpsError('permission-denied', 'Auth Error');
     }
 
-    const userDataRef = admin.firestore().collection('user_data');
+    return admin
+      .firestore()
+      .collection('user_data')
+      .listDocuments()
+      .then((userRefs) => {
+        return Promise.all(
+          userRefs.map((userRef) => {
+            return userRef.collection('feed_uris').listDocuments();
+          })
+        );
+      })
+      .then((uriRefs) => {
+        return Promise.all(
+          uriRefs.flat().map((uriRef) => {
+            return uriRef.get();
+          })
+        );
+      })
+      .then((uriSnapshots) => {
+        return Promise.all(
+          uriSnapshots
+            .filter((uriSnapshot) => uriSnapshot.exists)
+            .map((uriSnapshot) => {
+              const uriData = uriSnapshot.data()!;
+              const parser = new Parser();
+              return parser.parseURL(uriData['uri']);
+            })
+        );
+      })
+      .then((rssContents) => {
+        return Promise.all(
+          rssContents
+            .filter((rssContent) => rssContent.feedUrl != undefined)
+            .flatMap((rssContent) => {
+              // Index name does not accept a slash.
+              const uriCollectionRef = admin
+                .firestore()
+                .collection('rss_contents_store')
+                .doc('rss_content')
+                .collection(rssContent.feedUrl!.replace(/\//g, '_'));
 
-    userDataRef.get().then((snapshot) => {
-      snapshot.forEach((doc) => {
-        console.log(doc.id);
+              return rssContent.items
+                .filter((rssItem) => rssItem.link != undefined)
+                .map((rssItem) => {
+                  return uriCollectionRef
+                    .doc(rssItem.link!.replace(/\//g, '_'))
+                    .set(
+                      {
+                        title: rssItem.title,
+                        content: rssItem.content,
+                        pubDate: rssItem.pubDate,
+                      },
+                      { merge: true }
+                    );
+                });
+            })
+        );
       });
-    });
   }
 );
--- firebase/functions/test/index.spec.ts
+++ firebase/functions/test/index.spec.ts
@@ -1,9 +1,12 @@
 import { assert, expect } from 'chai';
+import * as admin from 'firebase-admin';
 import * as functions from 'firebase-functions';
-import * as firebaseFunctionsTest from 'firebase-functions-test';
+import firebaseFunctionsTest from 'firebase-functions-test';
 import { describe, it } from 'mocha';
 
 import * as sut from '../src/index';
+
+process.env.FIRESTORE_EMULATOR_HOST = 'localhost:8080';
 
 const test = firebaseFunctionsTest(
   {
@@ -21,12 +24,51 @@ describe('getRssContent', () => {
       'Auth Error'
     );
   });
+});
 
-  it('Throw error with wrong input to fetch', (done) => {
-    const wrapped = test.wrap(sut.getRssContent);
-    wrapped('wrongurl', { auth: true }).catch((error: TypeError) => {
-      expect(error.message).to.have.string('TypeError');
-      done();
+describe('updateRssContent', () => {
+  it('Throw error with un-authorized request', () => {
+    const wrapped = test.wrap(sut.updateRssContent);
+    assert.throws(
+      () => wrapped('example.com', {}),
+      functions.https.HttpsError,
+      'Auth Error'
+    );
+  });
+
+  it('Parse and update rss feed contents', async () => {
+    const wrapped = test.wrap(sut.updateRssContent);
+    const testUris = [
+      { uri: 'https://hnrss.org/frontpage' },
+      { uri: 'https://www.reddit.com/.rss' },
+    ];
+
+    await Promise.all(
+      testUris.map((doc) =>
+        admin
+          .firestore()
+          .collection('user_data')
+          .doc('dummy_user')
+          .collection('feed_uris')
+          .add(doc)
+      )
+    );
+
+    await wrapped({ auth: true });
+
+    const rssContentDocRef = admin
+      .firestore()
+      .collection('rss_contents_store')
+      .doc('rss_content');
+
+    await Promise.all(
+      testUris.map((doc) =>
+        rssContentDocRef
+          .collection(doc['uri'].replace(/\//g, '_'))
+          .listDocuments()
+      )
+    ).then((docs) => {
+      return Promise.all(docs.map((doc) => expect(doc.length > 0)));
     });
   });
 });
