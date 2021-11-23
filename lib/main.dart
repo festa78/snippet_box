353cc0260545dfa7b6c5c2fc886d67baed6d0c77
festa78 <nonamehorses78@gmail.com>
2021-11-23 09:49:13 +0900

    Enable switch to use firebase emulators on flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,25 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:cloud_functions/cloud_functions.dart';
 import 'package:flutter/material.dart';
 
 import 'package:myapp/widgets/snippet_box.dart';
 
-void main() => runApp(SnippetBox());
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  const firestoreEmulatorHost =
+      String.fromEnvironment('FIRESTORE_EMULATOR_HOST', defaultValue: null);
+  if (firestoreEmulatorHost != null) {
+    print('useFirestoreEmulator');
+    FirebaseFirestore.instance
+        .useFirestoreEmulator('localhost', int.parse(firestoreEmulatorHost));
+  }
+  const functionsEmulatorHost =
+      String.fromEnvironment('FUNCTIONS_EMULATOR_HOST', defaultValue: null);
+  if (functionsEmulatorHost != null) {
+    print('useFunctionsEmulator');
+    FirebaseFunctions.instance
+        .useFunctionsEmulator('localhost', int.parse(functionsEmulatorHost));
+  }
+
+  runApp(SnippetBox());
+}
