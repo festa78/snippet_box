ecfa73125808be929f979fd7f3a14120413ff533
festa78 <nonamehorses78@gmail.com>
2022-01-29 22:26:13 +0900

    move kde.py functions into tfidf.py

--- beam/trends/tfidf.py
+++ beam/trends/tfidf.py
@@ -28,8 +28,7 @@ from os import pipe
 import re
 
 import apache_beam as beam
-from apache_beam.io import ReadFromBigQuery, WriteToBigQuery
-from apache_beam.pvalue import AsSingleton
+from apache_beam.pvalue import AsIter, AsSingleton
 
 
 class TfIdf(beam.PTransform):
@@ -169,3 +168,54 @@ class TfIdf(beam.PTransform):
         | 'ComputeTf-idf' >> beam.FlatMap(compute_tf_idf))
 
     return word_to_uri_and_tfidf
+
+
+class TfidfVector(beam.PTransform):
+  """Compute TF-IDF vector per uri document.
+  It accumulate TF-IDF values for each uri document
+  and represent it as a vector.
+  The vector indices are unique word list over all documents.
+  """
+  def expand(self, word_uri_and_tfidf):
+    def transform_to_uri_word_and_tfidf(word_uri_and_tfidf):
+      """Transform from word_uri_and_tfidf to
+      {uri: (word, tfidf)} format, so that it can
+      group by uri.
+      """
+      (word, uri_and_tfidf) = word_uri_and_tfidf
+      return (uri_and_tfidf[0], (word, uri_and_tfidf[1]))
+
+    uri_word_and_tfidf = (
+      word_uri_and_tfidf
+      | 'Uri to WordTfidf map' >> beam.Map(transform_to_uri_word_and_tfidf)
+      | 'Group by uri' >> beam.GroupByKey()
+    )
+
+    def transform_to_vector_repr(uri_word_and_tfidf_list, unique_words):
+      """Form a vector whose element represents each unique word and
+      its tf-idf.
+      """
+      (uri, word_and_tfidf_list) = uri_word_and_tfidf_list
+      word_and_tfidf_dict = {word: tfidf for (word, tfidf) in word_and_tfidf_list}
+      tfidf_vector = [
+        word_and_tfidf_dict[word] if word in word_and_tfidf_dict.keys() else 0.0
+        for word in unique_words
+      ]
+
+      return (uri, tfidf_vector)
+
+    # NOTE: unique_words are not sorted.
+    # Does not matter unless we incrementally update the KDE values.
+    unique_words = (
+      word_uri_and_tfidf
+      | 'Get word list' >> beam.Keys()
+      | 'Get unique words' >> beam.Distinct()
+    )
+
+    tfidf_vector = (
+      uri_word_and_tfidf
+      | 'Compute tfidf vector by unique words' >> beam.Map(
+        transform_to_vector_repr, AsIter(unique_words))
+    )
+
+    return tfidf_vector
